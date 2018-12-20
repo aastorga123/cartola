b9f19d67d23f9b4949a8f4f15a261c02b0e601e8
elbraulio <brauliop.3@gmail.com>
2018-12-20 14:58:57 -0300

    lee path y file

--- lib/cartolabco.rb
+++ lib/cartolabco.rb
@@ -4,8 +4,13 @@ require 'bci_xls'
 module Cartolabco
   class BciParse
     def self.from_path(path)
-      CheckFile.new.check_file?(path)
-      BciXls.new(File.open(path, 'r'))
+      CheckFile.new.check_path?(path)
+      self.from_file(File.open(path, 'r'))
+    end
+
+    def self.from_file(file)
+      CheckFile.new.check_file?(file)
+      BciXls.new(file)
     end
   end
 end
